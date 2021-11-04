9b9bb9f80d6e4ac81e931e96db4d7634b8f823eb
dianaclx <dianaclxcoe@gmail.com>
2021-11-04 19:22:29 +0100

    modified html

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 
-const display1 = document.querySelector(".prevOperation");
-const display2 = document.querySelector(".input"); 
-const numbers =  document.querySelectorAll(".number"); 
-const decimal = document.querySelector(".decimal");
-const operations = document.querySelectorAll(".operator");
-const equal = document.querySelector(".equal");
-const entryDelete = document.querySelector(".backspace"); 
-const clear = document.querySelector(".delete"); 
-
-let topNum = ""; 
-let currNum = ""; 
-let result = null;
-let shouldResetScreen = false
+const numberButtons = document.querySelectorAll('[data-number]'); 
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton
